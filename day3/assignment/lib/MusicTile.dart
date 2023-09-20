88858f69d41ffa1af8c23efaa91da905b60f99c2
admin <onsk331@gmail.com>
2023-09-21 00:46:44 +0900

    3일차 과제완성

--- day3/assignment/lib/MusicTile.dart
+++ day3/assignment/lib/MusicTile.dart
@@ -1,17 +1,74 @@
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 
 class MusicTile extends StatelessWidget {
-  const MusicTile({super.key});
+  const MusicTile(
+      {super.key,
+      required this.imgCover,
+      required this.songName,
+      required this.artist,
+      required this.songPlayTime});
+  final String imgCover;
+  final String songName;
+  final String artist;
+  final String songPlayTime;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
+    return ListTile(
+      title: Text(
+        songName,
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
+        ),
+      ),
+      subtitle: Row(
         children: [
-          CircleAvatar(
-            backgroundImage: NetworkImage(''),
+          Icon(
+            Icons.check_circle,
+            color: Colors.white,
+            size: 15,
+          ),
+          SizedBox(
+            width: 7,
+          ),
+          Flexible(
+            child: Text(
+              artist,
+              style: TextStyle(fontSize: 15, color: Colors.grey),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          SizedBox(
+            width: 7,
+          ),
+          Icon(
+            Icons.circle,
+            color: Colors.grey,
+            size: 5,
+          ),
+          SizedBox(
+            width: 7,
+          ),
+          Text(
+            songPlayTime,
+            style: TextStyle(color: Colors.grey),
           )
         ],
+      ),
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(7),
+        child: Image.asset(imgCover),
+      ),
+      trailing: Icon(
+        Icons.more_vert,
+        color: Colors.white,
       ),
     );
   }
--- day3/assignment/lib/main.dart
+++ day3/assignment/lib/main.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
+import 'package:assignment/MusicTile.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,9 @@ class MyApp extends StatelessWidget {
             '아워리스트',
             style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
           ),
-          backgroundColor: Colors.black87,
+          backgroundColor: const Color.fromARGB(255, 20, 20, 20),
+          shadowColor: Colors.white,
+          elevation: 0.5,
           leading: Icon(Icons.arrow_back_ios),
           actions: [
             Padding(
@@ -28,8 +31,127 @@ class MyApp extends StatelessWidget {
                 padding: EdgeInsets.only(right: 10), child: Icon(Icons.search)),
           ],
         ),
-        body: ListView(
-          children: [],
+        body: Container(
+          color: Color.fromARGB(255, 25, 25, 25),
+          child: ListView(
+            children: [
+              MusicTile(
+                  imgCover: 'assets/images/music_come_with_me.png',
+                  songName: 'Come with me',
+                  artist: 'Surface 및 salem ilese',
+                  songPlayTime: '3:00'),
+              MusicTile(
+                  imgCover: 'assets/images/music_good_day.png',
+                  songName: 'Good day',
+                  artist: 'Surface',
+                  songPlayTime: '3:00'),
+              MusicTile(
+                  imgCover: 'assets/images/music_honesty.png',
+                  songName: 'Honesty',
+                  artist: 'Pink Sweat\$',
+                  songPlayTime: '3:09'),
+              MusicTile(
+                  imgCover: 'assets/images/music_i_wish_i_missed_my_ex.png',
+                  songName: 'I Wish I Missed My Ex',
+                  artist: '마할리아 버크마',
+                  songPlayTime: '3:24'),
+              MusicTile(
+                  imgCover: 'assets/images/music_plastic_plants.png',
+                  songName: 'Plastic Plants',
+                  artist: '마할리아 버크마',
+                  songPlayTime: '3:20'),
+              MusicTile(
+                  imgCover: 'assets/images/music_sucker_for_you.png',
+                  songName: 'Sucker for you',
+                  artist: '맷 테리',
+                  songPlayTime: '3:24'),
+              MusicTile(
+                  imgCover:
+                      'assets/images/music_summer_is_for_falling_in_love.png',
+                  songName: 'Summer is for falling in love',
+                  artist: 'Sarah Kang & Eye Love Brandon',
+                  songPlayTime: '3:00'),
+              MusicTile(
+                  imgCover: 'assets/images/music_these_days.png',
+                  songName:
+                      'These days(feat. Jess Glynne, Macklemore & Dan Caplen)',
+                  artist: 'Rudimental',
+                  songPlayTime: '3:00'),
+              MusicTile(
+                  imgCover: 'assets/images/music_you_make_me.png',
+                  songName: 'You Make Me',
+                  artist: 'DAY6',
+                  songPlayTime: '3:00'),
+              MusicTile(
+                  imgCover: 'assets/images/music_zombie_pop.png',
+                  songName: 'Zombie Pop',
+                  artist: 'DRP IAN',
+                  songPlayTime: '3:00'),
+            ],
+          ),
+        ),
+        bottomSheet: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              color: Color.fromARGB(255, 51, 51, 51),
+              height: 75,
+              child: ListTile(
+                title: Text(
+                  'You make me',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white),
+                ),
+                subtitle: Text(
+                  'DAY6',
+                  style: TextStyle(fontSize: 15, color: Colors.grey),
+                ),
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(7),
+                  child: Image.asset('assets/images/music_you_make_me.png'),
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Icon(
+                        Icons.skip_next,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 1,
+              alignment: Alignment.centerLeft,
+              child: Container(
+                width: 50,
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 2,
+          backgroundColor: Color.fromARGB(255, 51, 51, 51),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          elevation: 0,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: '둘러보기'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.library_music), label: '보관함')
+          ],
         ),
       ),
     );
