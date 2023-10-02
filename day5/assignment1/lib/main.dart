a29962d3966f2244811035f78aaabf0eac67643b
admin <onsk331@gmail.com>
2023-10-02 20:58:31 +0900

    5일차 2번과제 완성

--- day5/assignment1/lib/main.dart
+++ day5/assignment1/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, unnecessary_string_interpolations, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
@@ -67,14 +67,48 @@ class FirstPage extends StatelessWidget {
   }
 }
 
-class SecondPage extends StatelessWidget {
+class SecondPage extends StatefulWidget {
   const SecondPage({super.key});
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  final ScrollController _scrollController = ScrollController();
+  void scrollToTop() {
+    _scrollController.animateTo(0,
+        duration: Duration(milliseconds: 300), curve: Curves.linear);
+  }
+
+  var animalList = ['강아지', '고양이', '앵무새', '토끼', '오리', '거위', '원숭이'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('5일차 과제'),
+      ),
+      body: Center(
+        child: ListView.builder(
+          controller: _scrollController,
+          itemCount: animalList.length,
+          itemBuilder: (context, index) {
+            return Center(
+              child: Container(
+                padding: EdgeInsets.only(top: 200),
+                height: 300,
+                child: Text('${animalList[index]}'),
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          scrollToTop();
+        },
+        child: Icon(Icons.move_up_outlined),
       ),
     );
   }
